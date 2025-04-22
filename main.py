06dd2a8c2d818e226ac14343d621f01ac00db3b5
amine-e2r <153176619+amine-e2r@users.noreply.github.com>
2025-04-22 14:03:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -60,4 +60,6 @@ def point_fixe(C0, F, eps=eps, max_iter = max_iter):
         np.append(C,C[k-1] + h*F(k,C))
         if(k >= max_iter or np.norm(C[k] - C[k-1],2) < eps):
             break
+        k+=1
+    return C
 #methodes numeriques**********************
